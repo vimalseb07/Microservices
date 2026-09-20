13dc6bc42fafd3050a5d00ff111ebd31afef49f0
Vimal Sebastian <vimal.seb07@gmail.com>
2026-09-20 14:18:22 +0530

    Entity Id import correction

--- order_service/src/main/java/com/microservices/order_service/model/OrderItem.java
+++ order_service/src/main/java/com/microservices/order_service/model/OrderItem.java
@@ -1,15 +1,13 @@
 package com.microservices.order_service.model;
 
-import java.math.BigDecimal;
 import java.time.LocalDateTime;
-
-import org.springframework.data.annotation.Id;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.PrePersist;
